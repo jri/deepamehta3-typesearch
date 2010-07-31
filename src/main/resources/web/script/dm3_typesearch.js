6491ba5c72d3dc3dd82f8596a17584cb95ed4e3c
Jörg Richter <jri@deepamehta.de>
2010-07-31 17:13:49 +0200

    Adds a "By Type" search mode.

---
+++ src/main/resources/web/script/dm3_typesearch.js
@@ -0,0 +1,65 @@
+function dm3_typesearch() {
+
+
+
+    /**************************************************************************************************/
+    /**************************************** Overriding Hooks ****************************************/
+    /**************************************************************************************************/
+
+
+
+    /*** Provide "By Type" search mode ***/
+
+    this.init = function() {
+        $("#searchmode-select").append($("<option>").text("By Type"))
+    }
+
+    this.search_widget = function(searchmode) {
+        if (searchmode == "By Type") {
+            return create_type_menu("search-type-menu").dom
+        }
+    }
+
+    this.search = function(searchmode) {
+        if (searchmode == "By Type") {
+            // perform type search
+            var type_uri = ui.menu_item("search-type-menu").value
+            return dmc.get_topics_and_create_bucket(type_uri)
+        }
+    }
+
+    /***  Working together with the DM3 Type Editor plugin ***/
+
+    /**
+     * Once a "Topic Type" topic is updated we rebuild the type menu.
+     */
+    this.post_update_topic = function(topic, old_properties) {
+        if (topic.type_uri == "de/deepamehta/core/topictype/TopicType") {
+            update_type_menu()
+        }
+    }
+
+    /**
+     * Once a "Topic Type" topic is deleted we rebuild the type menu.
+     */
+    this.post_delete_topic = function(topic) {
+        if (topic.type_uri == "de/deepamehta/core/topictype/TopicType") {
+            update_type_menu()
+        }
+    }
+
+
+
+    /************************************************************************************************/
+    /**************************************** Custom Methods ****************************************/
+    /************************************************************************************************/
+
+
+
+    function update_type_menu() {
+        // Rebuilding the type menu is only required if the "By Type" searchmode is active.
+        if (ui.menu_item("searchmode-select").label == "By Type") {
+            rebuild_type_menu("search-type-menu")
+        }
+    }
+}
